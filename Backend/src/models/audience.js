f3945b8d1c13ace825d0fae6472aba574ccaa700
Aakash Jha <42407874+1tsak@users.noreply.github.com>
2023-05-21 19:25:02 +0530

    Fixed Schema Audience

--- Backend/src/models/audience.js
+++ Backend/src/models/audience.js
@@ -15,7 +15,8 @@ const actingOpeningSchema = mongoose.Schema({
   },
   category: {
     type: String,
-    required: true
+    required: true,
+    lowercase: true,
   },
   imageUrl: {
     type: String,
@@ -32,7 +33,6 @@ const actingOpeningSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
     lowercase: true,
     trim: true,
     match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
